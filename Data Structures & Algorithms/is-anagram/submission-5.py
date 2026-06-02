0dfb80076a012290d374047daad8af49d11aa94d
yonawid <yonatanmay262007@gmail.com>
2026-06-02 11:05:54 -0400

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        storage={}
+        storage2={}
+        for char in s:
+            storage[char]=storage.get(char,0)+1
+        for char in t:
+            storage2[char]=storage2.get(char,0)+1
+        return storage==storage2
